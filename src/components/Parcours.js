7f78660d3a2ba7b37d1c7e7b4ad1b4099de6ef1c
Bastien <neitsabast@gmail.com>
2022-11-13 11:54:40 +0100

    School json

--- src/components/Parcours.js
+++ src/components/Parcours.js
@@ -1,25 +1,26 @@
 import React from "react";
 import School from "./School";
+import schoolData from "../data/school.json";
 
 class Parcours extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {}
+
   render() {
     return (
       <div className="px-[20vw] pt-10 flex flex-col mx-auto items-center justify-center">
         <h2 className="text-4xl font-bold">Parcours scolaire</h2>
-        <School
-          imgSchool="https://www.iut.u-bordeaux.fr/general/wp-content/uploads/2017/03/iut_Bordeaux_RVB-01-e1488459784711.jpg"
-          textSchool="Licence Pro DAWIN - IUT de Bordeaux - Bac +3"
-          yearSchool="2022-2023"
-        />
-        <School
-          imgSchool="https://file.diplomeo-static.com/file/00/00/01/48/14869.svg"
-          textSchool="BTS SIO SLAM - Lycée Gustave Eiffel - Bac +2"
-          yearSchool="2020-2022"
-        />
+        {schoolData.map((school, i) => (
+          <School
+            key={i}
+            imgSchool={school.imgSchool}
+            textSchool={school.textSchool}
+            yearSchool={school.yearSchool}
+          />
+        ))}
       </div>
     );
   }
