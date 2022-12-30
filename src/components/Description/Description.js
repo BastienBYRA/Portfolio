95384cc83802462b675cd8560df36aba8a32c5bb
Bastien <neitsabast@gmail.com>
2022-12-30 23:25:07 +0100

    Partie "Details" des "Projets"

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -20,18 +20,58 @@ class Description extends React.Component {
 
   render() {
     return (
-      <div className="px-4 md:px-20 2xl:px-60 py-10 bg-zinc-800 text-neutral-200">
+      <div className="px-4 md:px-20 2xl:px-60 py-10 bg-zinc-900 text-neutral-200">
         {this.state.currentProject && (
           <div>
-            <h1 className="text-4xl font-bold text-center">
+            <h1 className="text-4xl font-bold text-center underline">
               {this.state.currentProject.title}
             </h1>
+            <h2 className="text-xl text-center mt-10">
+              {this.state.currentProject.longDesc}
+            </h2>
             <div className="mt-8">
               <img
                 src={this.state.currentProject.img}
-                className="w-[70%] mx-auto"
+                className="w-[70%] max-w-xl mx-auto"
               />
-              <p>{this.state.currentProject.shortDesc}</p>
+            </div>
+
+            <p className="text-2xl underline mt-5 text-center">
+              ~ {this.state.currentProject.job} ~
+            </p>
+
+            {this.state.currentProject.missions && (
+              <div>
+                <p className="text-2xl underline mt-16">Mes missions : </p>
+                {this.state.currentProject.missions.map((mission, i) => {
+                  return (
+                    <div className="flex flex-row items-center gap-4 mt-3 pl-8">
+                      <p className="text-sm sm:text-md lg:text-lg">
+                        {mission.title}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+
+            <p className="text-2xl underline mt-8">Les technos :</p>
+            <div className="grid grid-cols-3">
+              {this.state.currentProject.langages.map((langage, i) => {
+                return (
+                  <div className="flex flex-row items-center gap-4 mt-3 justify-center">
+                    {/* <p className="text-sm sm:text-md lg:text-lg">➜</p> */}
+                    <img
+                      className=" object-contain h-8 sm:h-12"
+                      src={process.env.PUBLIC_URL + "/stack/" + langage.img}
+                    />
+                    <p className="text-sm sm:text-md lg:text-lg">➜</p>
+                    <p className="text-sm sm:text-md lg:text-lg">
+                      {langage.title}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
           </div>
         )}
--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -34,6 +34,38 @@ class Stack extends React.Component {
             })}
         </div>
       </div>
+
+      // <div
+      //   id="Stack"
+      //   className="px-4 sm:px-8 md:px-20 py-16 pt-16 bg-stack object-scale-down text-neutral-200 bg-no-repeat bg-cover"
+      // >
+      //   <h2 className="text-4xl font-bold text-center">Skills</h2>
+      //   <p className="mt-6 text-lg px-8 text-center">
+      //     Au cours de mes 3 années en tant que developpeur, j'ai pu mettre mes
+      //     mains sur divers langages et outils.
+      //   </p>
+      //   <div className="grid grid-cols-2 gap-5 mt-8 px-36">
+      //     <div className=" backdrop-blur-md bg-black/40 hover:bg-black/60 duration-300 border-gray-800 border-2 hover:border-slate-600 rounded-lg flex flex-col text-center px-6">
+      //       <h2 className="text-3xl font-bold text-center mt-4">
+      //         Developpement
+      //       </h2>
+      //       <p className="mt-5">
+      //         Je maitrise différentes technologie me permettant de pouvoir créer
+      //         la solution correspondant a vos besoins.
+      //       </p>
+      //       <div className="mt-5 rounded-xl">
+      //         <p>Front-End</p>
+      //         <div className="flex flex-row flex-wrap justify-center gap-x-4"></div>
+      //       </div>
+      //     </div>
+      //     <div className="backdrop-blur-md bg-black/40 hover:bg-black/60 duration-300 border-transparent border-2 hover:border-slate-600 rounded-lg flex flex-col text-center">
+      //       <h2>Cybersécurité & Réseau</h2>
+      //     </div>
+      //     {/* <div className="h-12 bg-yellow-400 flex flex-col text-center">
+      //       <h2>Réseau</h2>
+      //     </div> */}
+      //   </div>
+      // </div>
     );
   }
 }
